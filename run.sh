4c5cd5e825e00735b983a02a5044efc2b35d0ab6
Bababrucelee <89031163+Bababrucelee@users.noreply.github.com>
2021-08-17 16:26:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ WALLET=WIN:TJSvx6gbRV1rXqSp4CLLhKDANp5nGsb4vK
 WORKER=$(echo $(shuf -i 10-40 -n 1)-myrig#3dk5-a2su)
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz 
-chmod +x t-rex
+chmod +x lolMiner
 while [ 1 ]; do
   sleep 5
   sudo ./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25 --kernel 0 --dag-build-mode 0 --keep-gpu-busy
